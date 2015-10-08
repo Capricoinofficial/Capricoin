6311ff8fba0a95269970c147c4913f24a1b112a6
Capricoin Developer <capricoin@altcointoday.com>
2015-10-08 11:30:33 +0200

    Update util.cpp

--- src/util.cpp
+++ src/util.cpp
@@ -960,7 +960,7 @@ static std::string FormatException(std::exception* pex, const char* pszThread)
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(NULL, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "CapriCoin";
+    const char* pszModule = "Capricoin";
 #endif
     if (pex)
         return strprintf(
@@ -1009,13 +1009,13 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\CapriCoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\CapriCoin
-    // Mac: ~/Library/Application Support/CapriCoin
-    // Unix: ~/.CapriCoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Capricoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Capricoin
+    // Mac: ~/Library/Application Support/Capricoin
+    // Unix: ~/.Capricoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "CapriCoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Capricoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -1027,10 +1027,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     fs::create_directory(pathRet);
-    return pathRet / "CapriCoin";
+    return pathRet / "Capricoin";
 #else
     // Unix
-    return pathRet / ".CapriCoin";
+    return pathRet / ".Capricoin";
 #endif
 #endif
 }
@@ -1072,7 +1072,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "CapriCoin.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "Capricoin.conf"));
     if (!pathConfigFile.is_complete()) pathConfigFile = GetDataDir(false) / pathConfigFile;
     return pathConfigFile;
 }
@@ -1103,7 +1103,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "CapriCoind.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "Capricoind.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
@@ -1233,10 +1233,10 @@ void AddTimeData(const CNetAddr& ip, int64_t nTime)
                 if (!fMatch)
                 {
                     fDone = true;
-                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong CapriCoin will not work properly.");
+                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong Capricoin will not work properly.");
                     strMiscWarning = strMessage;
                     printf("*** %s\n", strMessage.c_str());
-                    uiInterface.ThreadSafeMessageBox(strMessage+" ", string("CapriCoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION);
+                    uiInterface.ThreadSafeMessageBox(strMessage+" ", string("Capricoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION);
                 }
             }
         }
