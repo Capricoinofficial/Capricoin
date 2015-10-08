6deddff3c63dc0a6e19a65a47cc5ea3110a5fa6f
Capricoin Developer <capricoin@altcointoday.com>
2015-10-08 11:33:03 +0200

    Update qrcodedialog.cpp

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -83,7 +83,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("CapriCoin:%1").arg(address);
+    QString ret = QString("Capricoin:%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
