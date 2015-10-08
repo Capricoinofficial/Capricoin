2e6a3752ab9415e15eb408b683e782aba27fc49b
Capricoin Developer <capricoin@altcointoday.com>
2015-10-08 11:27:57 +0200

    Update bitcoin.cpp

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -106,7 +106,7 @@ static std::string Translate(const char* psz)
 static void handleRunawayException(std::exception *e)
 {
     PrintExceptionContinue(e, "Runaway exception");
-    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. CapriCoin can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
+    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. Capricoin can no longer continue safely and will quit.") + QString("\n\n") + QString::fromStdString(strMiscWarning));
     exit(1);
 }
 
@@ -136,7 +136,7 @@ int main(int argc, char *argv[])
     {
         // This message can not be translated, as translation is not initialized yet
         // (which not yet possible because lang=XX can be overridden in bitcoin.conf in the data directory)
-        QMessageBox::critical(0, "CapriCoin",
+        QMessageBox::critical(0, "Capricoin",
                               QString("Error: Specified data directory \"%1\" does not exist.").arg(QString::fromStdString(mapArgs["-datadir"])));
         return 1;
     }
@@ -144,12 +144,12 @@ int main(int argc, char *argv[])
 
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
-    app.setOrganizationName("CapriCoin");
+    app.setOrganizationName("Capricoin");
     //XXX app.setOrganizationDomain("");
     if(GetBoolArg("-testnet")) // Separate UI settings for testnet
-        app.setApplicationName("CapriCoin-Qt-testnet");
+        app.setApplicationName("Capricoin-Qt-testnet");
     else
-        app.setApplicationName("CapriCoin-Qt");
+        app.setApplicationName("Capricoin-Qt");
 
     // ... then GUI settings:
     OptionsModel optionsModel;
