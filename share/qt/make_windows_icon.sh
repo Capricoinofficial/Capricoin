961a1c87a63073bcd328cff7fa91b806ed09dab1
Capricoin Official Developer <42577904+Capricoin-dev@users.noreply.github.com>
2018-12-11 09:36:08 +0100

    Fix windows icon script

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/novacoin.ico
+ICON_DST=../../src/qt/res/icons/Capricoin.ico
 
-convert ../../src/qt/res/icons/novacoin-16.png ../../src/qt/res/icons/novacoin-32.png ../../src/qt/res/icons/novacoin-48.png ${ICON_DST}
+convert ../../src/qt/res/icons/Capricoin-16.png ../../src/qt/res/icons/Capricoin-128.png ../../src/qt/res/icons/Capricoin-256.png ${ICON_DST}
